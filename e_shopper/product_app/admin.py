e1e02dc0fdf66853e60fe37150e103dc94198968
Bhavin Chavada <bhavinchavada242846@gmail.com>
2024-11-16 17:31:50 +0530

    set ProductImage in ProductModel so user can add multiple image in product

--- e_shopper/product_app/admin.py
+++ e_shopper/product_app/admin.py
@@ -1,13 +1,18 @@
 from django.contrib import admin
-from product_app.models import ProductModel,ProductSize
+from product_app.models import ProductModel,ProductSize,ProductImage
 
 # Register your models here.
 class ProductSizeInline(admin.TabularInline):
     model  = ProductSize
     extra = 1
     fields = ['size', 'additional_price'] 
+
+class ProductImagesInline(admin.TabularInline):
+    model = ProductImage
+    extra = 1
+    fields = ['image']
     
 @admin.register(ProductModel)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ['ProductName','ProductImage','category','subcategory','ProductPrice','is_active']
-    inlines = [ProductSizeInline]
+    inlines = [ProductSizeInline,ProductImagesInline]
