f66dcc94f4cc01f7819a7903adc9ce909f80b0fc
Bhavin Chavada <bhavinchavada242846@gmail.com>
2024-11-20 12:16:44 +0530

    make change in settings.py

--- e_shopper/E_shopper/settings.py
+++ e_shopper/E_shopper/settings.py
@@ -152,8 +152,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 # vercel statis config
+STATIC_URL = '/static/' 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static') 
-STATIC_URL = '/staticfiles/' 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 MEDIA_URL='/media/'
 MEDIA_ROOT= os.path.join(BASE_DIR,'media')
