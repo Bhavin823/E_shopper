eb4a7adc619fd5e0e2a8bf34c51458d112ba3d6c
Bhavin Chavada <bhavinchavada242846@gmail.com>
2024-11-21 13:47:47 +0530

    resolve banner issue

--- e_shopper/base_app/views.py
+++ e_shopper/base_app/views.py
@@ -3,6 +3,7 @@ from category_app.views import CategoryView,Cat_Subcat_Nav_View
 from cart_app.views import cart_total_quantity
 from product_app.models import ProductModel
 from category_app.models import CategoryModel
+from django.templatetags.static import static
 
 # Create your views here.
 def baseview(request):
@@ -13,6 +14,7 @@ def baseview(request):
     return render(request,'base_app/base.html',context)
 
 def homeview(request):
+    banner_image_url = request.build_absolute_uri(static('images/shop/eshopper_banner.png'))
     categories = CategoryView()
     # fetch 6 random products for recommended items
     recommended_products = ProductModel.objects.all().filter(is_active=True).order_by('?')[:6].prefetch_related('images')
@@ -25,7 +27,8 @@ def homeview(request):
         'categories': categories,
         'cat_sub_nav' : cat_subcat_for_nav,
         'cart_total_quantity':cart_total_quan,
-        'recommended_products': recommended_products
+        'recommended_products': recommended_products,
+        'banner_image_url':banner_image_url,
     }
     return render(request,'base_app/home.html',context)
 
