4b5c2443f7924273c2deb68c2182684fb61d8920
Bhavin Chavada <bhavinchavada242846@gmail.com>
2024-11-25 18:59:05 +0530

    make some changes not nessesary

--- e_shopper/payment_app/urls.py
+++ e_shopper/payment_app/urls.py
@@ -7,16 +7,13 @@ from payment_app import views
 app_name = 'payment_app'
 
 urlpatterns = [
-    
-	# path('initialize_razorpay/<int:order_id>/', views.initialize_razorpay, name='initialize_razorpay'),
-    # path('razorpay_payment_success/', views.razorpay_payment_success, name='razorpay_payment_success'),
-    
-    path('payment_success_page/',views.payment_success_page,name='payment_success_page'),
-    # path('payment_faliure_page/',views.payment_failure_page,name='payment_failure_page'),
-
-
-    # new flow 
+    # payment intialize
     path("payment/<int:order_id>/", views.order_payment, name="payment"),
+    
+    # after payment 
     path("callback/", views.callback, name="callback"),
-
+    
+    # after payment status call 
+    path('payment_success_page/',views.payment_success_page,name='payment_success_page'),
+    
 ]
